b310c98190320d8f5d4b7b9e4b93d73444d996c3
Mihir Ghawghawe <Mihirg.301@gmail.com>
2022-11-18 10:40:02 -0600

    Commented Autonomous Code-Removed unneeded Auto Files

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4008Auto1ParkingRedLeft.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4008Auto1ParkingRedLeft.java
@@ -18,25 +18,25 @@ public class Team4008Auto1ParkingRedLeft extends LinearOpMode{
         waitForStart();
 
         //Strafes Left
-        double distance = 20;
-        multy = 0.4;
+        double distance = 20; //Distance in inches to strafe
+        multy = 0.4; //Power setting to all motors
         robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         double tick = (distance * 537.7)/(4 * Math.PI);
         Time.reset();
-        robot.DriveRightFront.setPower(-multy);
-        robot.DriveLeftFront.setPower(multy);
-        robot.DriveRightBack.setPower(multy);
+        robot.DriveRightFront.setPower(-multy); //Setting the power to (multy) variable created above
+        robot.DriveLeftFront.setPower(multy); //Link to Wheel Direction Mapping Below
+        robot.DriveRightBack.setPower(multy); //https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
         robot.DriveLeftBack.setPower(-multy);
-        while(opModeIsActive() && Time.milliseconds() < 2000 && robot.DriveLeftFront.getCurrentPosition() < tick) {
-            telemetry.addData("Encoder Val", robot.DriveLeftFront.getCurrentPosition());
-            telemetry.update();
+        while(opModeIsActive() && Time.milliseconds() < 2000 && robot.DriveLeftFront.getCurrentPosition() < tick) { //If Encoder is outputting incorrectly, motor will automatically stop if time in miliseconds has been reached
+            telemetry.addData("Encoder Val", robot.DriveLeftFront.getCurrentPosition()); //Printing Telemtry values to the phone
+            telemetry.update(); //Constantly updates telemetry to the phone
         }
-        robot.DriveRightFront.setPower(0);
+        robot.DriveRightFront.setPower(0); //Sets power to all motors to 0 after desired distance has been reached
         robot.DriveLeftFront.setPower(0);
         robot.DriveRightBack.setPower(0);
         robot.DriveLeftBack.setPower(0);
-        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); //Stops and resets the encoder to the 0 value for next use
         robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         sleep(1000);
     }
